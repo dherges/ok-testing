75ec004e2386a92872bd38b1896a42fa02f3a8b4
dherges <david@spektrakel.de>
2016-07-18 18:34:44 +0200

    Third feature: adds a MockWebServer to the scenery, test case demonstrates how to script mocked responses, dispatches a HTTP request to the mock backend, asserts that our client gets a 200 Ok with application/json payload

--- src/test/java/oktesting/messaging/MessagesApiTest.java
+++ src/test/java/oktesting/messaging/MessagesApiTest.java
@@ -1,11 +1,21 @@
 package oktesting.messaging;
 
+import okhttp3.ResponseBody;
+import okhttp3.mockwebserver.MockResponse;
+import okhttp3.mockwebserver.MockWebServer;
+import org.junit.Rule;
 import org.junit.Test;
+import retrofit2.Response;
 
+
+import java.io.IOException;
 
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
 public class MessagesApiTest {
+
+    @Rule
+    public MockWebServer mockBackend = new MockWebServer();
 
     @Test
     public void builder_returnsInstance() {
@@ -14,6 +24,28 @@ public class MessagesApiTest {
                 .build();
 
         assertThat(messagesApi).isNotNull();
+    }
+
+    @Test
+    public void findMessage_receivesAScriptedResponseFromMockBackend() throws IOException {
+        mockBackend.enqueue(
+            new MockResponse()
+                .setBody("{\"text\":\"hello!\"}")
+                .setResponseCode(200)
+                .addHeader("Content-Type", "application/json;charset=utf-8")
+        );
+
+        final MessagesApi msgApi = new MessagesApi.Builder()
+                .baseUrl(mockBackend.url("/").toString())
+                .build();
+        final Response<Message> response = msgApi.findMessage("123").execute();
+
+        assertThat(response.code()).isEqualTo(200);
+        assertThat(response.headers().get("Content-Type")).contains("application/json");
+
+        final ResponseBody rawResponseBody = response.raw().body();
+        assertThat(rawResponseBody.contentType().type()).isEqualTo("application");
+        assertThat(rawResponseBody.contentType().subtype()).isEqualTo("json");
     }
 
 }
