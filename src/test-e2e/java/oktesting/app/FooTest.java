c1fbf4e3f7aa22a150c313a61eba1f04b651521c
dherges <david@spektrakel.de>
2016-07-19 22:46:11 +0200

    Part 2, first thing: add e2e tests to gradle build

---
+++ src/test-e2e/java/oktesting/app/FooTest.java
@@ -0,0 +1,14 @@
+package oktesting.app;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class FooTest {
+
+    @Test
+    public void testsAreWorking() {
+        assertThat(true).isFalse();
+    }
+
+}
